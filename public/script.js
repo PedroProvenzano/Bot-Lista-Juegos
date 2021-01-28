ffb4b6ddfe33ce751ebe5d13011f538d058da42e
Pedro Provenzano <pedroprov0@gmail.com>
2021-01-28 20:47:54 -0300

    Cambios en estilado

--- public/script.js
+++ public/script.js
@@ -568,7 +568,7 @@ var tag = document.createElement('script');
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
           height: '1000',
-          width: '1000',
+          width: '1200',
           videoId: 'doOXUOlalmU',
           events: {
             'onStateChange': onPlayerStateChange
